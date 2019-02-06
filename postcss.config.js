49e32f1dc63679359c31b231b09a7e2db717507e
kimihito <tatsurotamashiro@gmail.com>
2019-02-06 22:09:52 +0900

    :fire: ByeBye tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
-var tailwindcss = require('tailwindcss')
 module.exports = {
   plugins: [
-    tailwindcss('app/javascript/styles/tailwindcss.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
