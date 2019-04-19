1c126057c39fb5923ee519138e03125bd355f842
kimihito <tatsurotamashiro@gmail.com>
2019-04-19 22:14:50 +0900

    :sparkles: bin/rails g controller tracks

---
+++ test/controllers/tracks_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class TracksControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:idoneman)
+    @project = projects(:project_1)
+    sign_in @user
+  end
+
+  test "should create track" do
+    assert_difference "Track.count" do
+      post tracks_url, params: { track: { raw_body: "##{@project.title} track", owner: @user }}
+    end
+
+    assert_equal Track.recent.first.raw_body, "##{@project.title} track"
+    assert_equal @project, Track.recent.first.project
+  end
+end
