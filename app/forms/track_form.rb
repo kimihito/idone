d04c3cb906a2c2ae68afb04b15ddbdc43e04c69b
kimihito <tatsurotamashiro@gmail.com>
2019-04-20 20:24:49 +0900

    :sparkles: Add track_form

---
+++ app/forms/track_form.rb
@@ -0,0 +1,21 @@
+class ContributionForm < Patterns::Form
+  include Twitter::TwitterText::Extractor
+
+  param_key "track"
+  attribute :raw_body, String
+  attribute :owner_id, String
+
+  validates :raw_body, :owner_id, presence: true
+
+  private
+
+  def persist
+    project = project_from_raw_body
+    resource.update_attributes(attributes.merge(project_id: project&.id))
+  end
+
+  def project_from_raw_body
+    project_titles = extract_hashtags(raw_body)
+    Project.find_by(title: project_titles)
+  end
+end
