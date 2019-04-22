ac7d21f0104afde38702beeca5bdd590ecfc9471
kimihito <tatsurotamashiro@gmail.com>
2019-04-22 15:16:01 +0900

    :fire: Unused targets

--- app/javascript/controllers/emoji_parser_controller.js
+++ app/javascript/controllers/emoji_parser_controller.js
@@ -2,8 +2,6 @@ import { Controller } from 'stimulus'
 import twemoji from 'twemoji'
 
 export default class extends Controller {
-  static targets = ["rawText"]
-
   connect() {
     twemoji.parse(this.element, { folder: 'svg', ext: '.svg'})
   }
