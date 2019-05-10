64922a1d4151053a23ccd22bc7fd52c98c78ef36
kimihito <tatsurotamashiro@gmail.com>
2019-05-10 15:14:29 +0900

    :sparkles: Set track on root page

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -2,9 +2,7 @@ import { Controller } from 'stimulus'
 
 import Tribute from 'tributejs'
 
-import Qs from 'qs'
-
-import { throttle, isEmpty } from 'lodash'
+import { throttle } from 'lodash'
 
 import { extractHashtags } from 'twitter-text'
 
@@ -18,18 +16,17 @@ export default class extends Controller {
   static targets = ["rawBody", "projectId", "error"]
   initialize() {
     this.validProject = throttle(this.validProject, 1000)
-    console.log(this.validProject)
   }
 
   async connect() {
-    await this.fetchProjects()
+    projects = await this.fetchProjects()
     projectNames = projects.map(project => { return project.attributes.title })
 
     this.tribute = new Tribute({
       trigger: '#',
       values: projects,
       selectTemplate: (item) => { return `#${item.original.attributes.title}` },
-      lookup: (project, mentionText) => { return project.attributes.title },
+      lookup: (project, _mentionText) => { return project.attributes.title },
     })
     this.tribute.attach(this.rawBodyTarget)
     this.errorTarget.style.display = 'none'
@@ -39,18 +36,18 @@ export default class extends Controller {
     this.tribute.detach(this.rawBodyTarget)
   }
 
-  fetchProjects() {
-    return fetch(`${URL}`).then(response => {
-      return response.json()
-    }).then(json => {
-      projects = json.data
-    }).catch(error => {
-      console.error('error:', error)
-    })
+  async fetchProjects() {
+    try {
+      const response = await fetch(`${URL}`);
+      const json = await response.json();
+      return json.data;
+    }
+    catch (error) {
+      console.error('error:', error);
+    }
   }
 
   validProject(event) {
-      console.log(this.tribute.isActive)
       if(this.rawBodyTarget.value.length < 2) {
         this.errorTarget.style.display = 'none'
         event.preventDefault()
@@ -60,22 +57,28 @@ export default class extends Controller {
       const validProjectNames = hashTags.filter(tag => { return projectNames.includes(tag) })
       if(!validProjectNames.length) {
         this.appendErrorMessage('参加しているプロジェクトがありません')
+        this.clearProjectId()
         event.preventDefault();
         return
       }
       if (validProjectNames.length > 1) {
         this.appendErrorMessage('複数のプロジェクトに一つの投稿はできません')
+        this.clearProjectId()
         event.preventDefault();
         return
       }
       this.errorTarget.style.display = 'none'
       this.errorTarget.innerText = ''
-      // this.projectIdTarget.value =
+      this.projectIdTarget.value  = projects.find(project => { return project.attributes.title === validProjectNames[0] }).id
       event.preventDefault()
   }
 
   appendErrorMessage(text) {
     this.errorTarget.style.display = ''
     this.errorTarget.innerText = text
+  }
+
+  clearProjectId() {
+    this.projectIdTarget.value = ''
   }
 }
