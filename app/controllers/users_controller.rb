a6b3224fa19169eaac7986727a92af0ed684f63b
kimihito <tatsurotamashiro@gmail.com>
2019-09-02 17:21:40 +0900

    :sparkles: Add /usrs/:name path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     @users = User.all
     authorize(@users)
   end
+
+  def show(name)
+    @user = User.find_by(name: name)
+    authorize(@user)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get users_url
     assert_response :success
   end
 
+  test 'should get show' do
+    user = users(:idoneman)
+    get user_url(user)
+    assert_response :success
+  end
 end
