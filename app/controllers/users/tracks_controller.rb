f863792f2d68375616cf3a6d33084b97915ba5c0
kimihito <tatsurotamashiro@gmail.com>
2019-09-21 22:11:07 +0900

    Add tracks layout on user detail page

--- app/controllers/users/tracks_controller.rb
+++ app/controllers/users/tracks_controller.rb
@@ -1,7 +1,8 @@
 class Users::TracksController < ApplicationController
   def index(user_name:)
     @user = User.includes(tracks: :project).find_by!(name: user_name)
-    @pagy, @tracks = pagy(@user.tracks.order(created_at: :desc))
+    @pagy, @tracks = pagy(@user.tracks.recent)
+    @tracks_by_dates = @tracks.group_by_day(reverse: true, &:created_at)
     skip_authorization
   end
 end
