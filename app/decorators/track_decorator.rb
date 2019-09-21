8fa0a306d1ff4cfc42117a54c6ed5ab243f21548
kimihito <tatsurotamashiro@gmail.com>
2019-09-21 21:53:30 +0900

    Update track layout

--- app/decorators/track_decorator.rb
+++ app/decorators/track_decorator.rb
@@ -3,7 +3,7 @@ module TrackDecorator
   include Rails.application.routes.url_helpers
 
   def body
-    html = auto_link_hashtags(raw_body, { :hashtag_url_block => lambda {  |_| project_tracks_path(self.project) } , hashtag_class: 'Label Label--gray'})
+    html = auto_link_hashtags(raw_body, { :hashtag_url_block => lambda {  |_| project_tracks_path(self.project) } , hashtag_class: 'Label Label--outline bg-blue-light border-white-fade rounded-1 text-bold link-hover-blue' })
     raw html
   end
 end
