a4d20d11c45143b91684575deed93eb66c3a244f
kimihito <tatsurotamashiro@gmail.com>
2019-09-23 00:08:07 +0900

    Update test for user detail page

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require "test_helper"
+require 'supports/login_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include LoginHelper
   driven_by :selenium, using: :headless_chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
 end
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,8 +1,6 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  include Devise::Test::IntegrationHelpers
-
   def setup
     @user = users(:idoneman)
     @no_track_user = users(:no_track_user)
@@ -11,20 +9,20 @@ class UsersTest < ApplicationSystemTestCase
   test 'all user can show' do
     visit user_url(@user)
     assert_text @user.name
-    assert_text @user.created_at
-    assert_text "#{@user.tracks.size} Done"
-    assert_text "#{@user.projects.size} Projects"
+    assert_text I18n.l(@user.created_at.to_date)
+    assert_text "#{I18n.t('components.users.show.navigation.track')}#{@user.tracks.size}"
+    assert_text "#{I18n.t('components.users.show.navigation.project')}#{@user.projects.size}"
   end
 
   test 'login user can show edit button in same user page' do
-    sign_in(@user)
+    login_user(@user.name)
     visit user_url(@user)
-    assert_button '編集'
+    assert_link I18n.t('components.users.show.layout.edit_profile')
   end
 
   test 'display empty state user has no track' do
     visit user_url(@no_track_user)
-    assert_text "#{@no_track_user.tracks.size} Done"
-    assert_text "No Done"
+    assert_text "#{I18n.t('components.users.show.navigation.track')}#{@no_track_user.tracks.size}"
+    assert_text I18n.t('components.empty_state.track.header')
   end
 end
