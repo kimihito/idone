4284881e541e54ffc8135e9cf027eba60cdfb691
kimihito <tatsurotamashiro@gmail.com>
2019-09-29 15:05:40 +0900

    :recycle: Merge code to display icon or identicon

--- app/javascript/controllers/avatar_controller.ts
+++ app/javascript/controllers/avatar_controller.ts
@@ -6,6 +6,7 @@ import sprites from '@dicebear/avatars-identicon-sprites'
 export default class extends Stimulus.Controller {
   public connect() {
     if (!(this.element instanceof HTMLImageElement)) return
+    if (this.element.getAttribute('src')) return
     const seed: string = this.data.get('seed')
     const avatars = new Avatars(sprites())
     const svg = avatars.create(seed)
