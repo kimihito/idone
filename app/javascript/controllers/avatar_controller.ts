176684b6be46464beda4692657bffcc097884d29
kimihito <tatsurotamashiro@gmail.com>
2019-09-29 11:02:58 +0900

    :lipstick: empty image layout

---
+++ app/javascript/controllers/avatar_controller.ts
@@ -0,0 +1,14 @@
+import * as Stimulus from 'stimulus'
+import Avatars from '@dicebear/avatars'
+import sprites from '@dicebear/avatars-identicon-sprites'
+
+
+export default class extends Stimulus.Controller {
+  public connect() {
+    if (!(this.element instanceof HTMLImageElement)) return
+    const seed: string = this.data.get('seed')
+    const avatars = new Avatars(sprites())
+    const svg = avatars.create(seed)
+    this.element.setAttribute('src', `data:image/svg+xml, ${encodeURIComponent(svg)}`)
+  }
+}
