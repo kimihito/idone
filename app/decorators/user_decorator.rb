ff54f3986aa3561c37e535933b654d2c52acadc3
kimihito <tatsurotamashiro@gmail.com>
2019-10-06 06:01:43 +0900

    :bug: Add url_for method

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,5 @@
 module UserDecorator
   def icon_image(options = {})
-    image_tag 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', options.merge(data: { controller: 'avatar', 'avatar-seed': name, src: "#{icon.attached? ? icon.variant(combine_options: { resize: '150x150^', crop: '150x150+0+0', gravity: :center }) : '' }" })
+    image_tag 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', options.merge(data: { controller: 'avatar', 'avatar-seed': name, src: "#{icon.attached? ? url_for(icon.variant(combine_options: { resize: '150x150^', crop: '150x150+0+0', gravity: :center })) : '' }" })
   end
 end
