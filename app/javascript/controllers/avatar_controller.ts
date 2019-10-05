1228b0bcc6ff500b547a2fd8a65b41a89f092e5b
kimihito <tatsurotamashiro@gmail.com>
2019-10-06 02:16:56 +0900

    :recycle: Add UserDecorator

--- app/javascript/controllers/avatar_controller.ts
+++ app/javascript/controllers/avatar_controller.ts
@@ -6,10 +6,10 @@ import sprites from '@dicebear/avatars-identicon-sprites'
 export default class extends Stimulus.Controller {
   public connect() {
     if (!(this.element instanceof HTMLImageElement)) return
-    if (this.element.getAttribute('src')) return
+    if (this.element.getAttribute('data-src')) return
     const seed: string = this.data.get('seed')
     const avatars = new Avatars(sprites())
     const svg = avatars.create(seed)
-    this.element.setAttribute('src', `data:image/svg+xml, ${encodeURIComponent(svg)}`)
+    this.element.setAttribute('data-src', `data:image/svg+xml, ${encodeURIComponent(svg)}`)
   }
 }
