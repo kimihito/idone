1c2a1eadfda0422e9e02286a206f90190c92df8a
kimihito <tatsurotamashiro@gmail.com>
2019-10-10 13:46:14 +0900

    :heavy_plus_sign: Introduce puma_worker_killer gem

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,10 @@
 # Allow puma to be restarted by `rails restart` command.
+before_fork do
+  PumaWorkerKiller.config do |config|
+    config.ram = Integer(ENV.fetch('MEMORY_AVAILABLE', 512))
+  end
+  PumaWorkerKiller.enable_rolling_restart
+end
+
 plugin :tmp_restart
 plugin :heroku
