f4d72926a83c8d674552bd887f0bb077d0686ad1
kimihito <tatsurotamashiro@gmail.com>
2019-10-19 14:03:39 +0900

    bin/rails g migration AddDescriptionToProjects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,11 +2,12 @@
 #
 # Table name: projects
 #
-#  id         :uuid             not null, primary key
-#  title      :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  owner_id   :uuid             not null
+#  id          :uuid             not null, primary key
+#  title       :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  owner_id    :uuid             not null
+#  description :text             default(""), not null
 #
 # Indexes
 #
