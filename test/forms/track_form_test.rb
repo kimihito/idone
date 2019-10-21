3f2b864947fb53854ff73bdac5912ff643c52f2e
kimihito <tatsurotamashiro@gmail.com>
2019-10-21 18:05:56 +0900

    :sparkles: Touch project when track is created

--- test/forms/track_form_test.rb
+++ test/forms/track_form_test.rb
@@ -27,4 +27,31 @@ class TrackFormTest < ActiveSupport::TestCase
       form.save
     end
   end
+
+  test "should update project updated_at when track is created" do
+    travel_to Time.current.yesterday do
+      @project = Project.create(title: 'title title', owner: @owner)
+    end
+    old_project_updated_at = @project.updated_at
+    form = TrackForm.new(Track.new, {raw_body: "##{@project.title} body", owner_id: @owner.id, project_id: @project.id })
+    form.save
+    track_updated_at = form.send(:resource).updated_at
+    new_project_updated_at = @project.reload.updated_at
+    assert_not_equal old_project_updated_at.to_s, new_project_updated_at.to_s
+    assert_equal track_updated_at.to_s, new_project_updated_at.to_s
+  end
+
+  test "should not update project updated_at when track is updated" do
+    travel_to Time.current.yesterday do
+      @project = Project.create(title: 'new_project', owner: @owner)
+      @track = Track.create(raw_body: "##{@project.title} exists", owner: @owner, project: @project)
+    end
+    old_project_updated_at = @project.updated_at
+    form = TrackForm.new(@track, {raw_body: "##{@project.title} body", owner_id: @owner.id, project_id: @project.id })
+    form.save
+    track_updated_at = form.send(:resource).updated_at
+    new_project_updated_at = @project.reload.updated_at
+    assert_equal old_project_updated_at.to_s, new_project_updated_at.to_s
+    assert_not_equal track_updated_at.to_s, new_project_updated_at.to_s
+  end
 end
