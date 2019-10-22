6cfde10b243ad0d6ccd47e80306ac750028433b5
kimihito <tatsurotamashiro@gmail.com>
2019-10-22 16:59:49 +0900

    :construction: Fix tests

--- app/models/track.rb
+++ app/models/track.rb
@@ -21,7 +21,7 @@ class Track < ApplicationRecord
   include PublicActivity::Common
 
   belongs_to :owner, class_name: 'User', inverse_of: :tracks, touch: true
-  belongs_to :project_tag
+  has_one :project_tag
   has_one :project, through: :project_tag
 
   scope :recent, -> { order('created_at DESC') }
