ab9ce15c50c8dd1037d2485c12345521f526d572
kimihito <tatsurotamashiro@gmail.com>
2019-10-27 17:23:23 +0900

    :sparkles: bin/rails g model Tag

---
+++ db/migrate/20191027082151_create_tags.rb
@@ -0,0 +1,13 @@
+class CreateTags < ActiveRecord::Migration[5.2]
+  def change
+    remove_reference :project_tags, :track, foreign_key: true, null: true
+
+    create_table :tags do |t|
+      t.string :name, null: false, index: true
+
+      t.timestamps
+    end
+
+    add_reference :project_tags, :tag, foreign_key: true, null: false
+  end
+end
