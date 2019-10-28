b8f360a0f88d870038842dc0f602dfa820dbe0f0
kimihito <tatsurotamashiro@gmail.com>
2019-10-28 16:24:29 +0900

    :bug: Fix TrackDecorator#body

--- app/decorators/track_decorator.rb
+++ app/decorators/track_decorator.rb
@@ -3,7 +3,11 @@ module TrackDecorator
   include Rails.application.routes.url_helpers
 
   def body
-    html = auto_link_hashtags(raw_body, { :hashtag_url_block => lambda {  |_| project_tracks_path(self.project) } , hashtag_class: 'Label Label--outline bg-blue-light border-white-fade rounded-1 text-bold link-hover-blue' })
+    html = if project
+      auto_link_hashtags(raw_body, { :hashtag_url_block => lambda {  |_| project_tracks_path(self.project) } , hashtag_class: 'Label Label--outline bg-blue-light border-white-fade rounded-1 text-bold link-hover-blue' })
+    else
+      raw_body
+    end
     raw html
   end
 end
