aa17cd729497d47a30fe1f992044571b5f13dd27
kimihito <tatsurotamashiro@gmail.com>
2019-11-05 09:11:20 +0900

    :heavy_plus_sign: WIP: Introduce Choices.js

--- app/javascript/controllers/tag_controller.ts
+++ app/javascript/controllers/tag_controller.ts
@@ -1,8 +1,33 @@
 import * as Stimulus from 'stimulus'
-import Tagify from '@yaireo/tagify'
+import Choices from 'choices.js'
+import feather from 'feather-icons'
+
 
 export default class extends Stimulus.Controller {
   public connect() {
-    new Tagify(<HTMLInputElement>this.element)
+    new Choices(<HTMLInputElement>this.element, {
+      removeItemButton: true,
+      duplicateItemsAllowed: false,
+      classNames: {
+        containerInner: 'form-control'
+      },
+      callbackOnCreateTemplates: template => {
+        return {
+          item: (_, data) => {
+            return template(`
+              <span class='Label Label--outline bg-blue-light border-white-fade rounded-1 text-bold mr-1 mb-1'
+                data-item
+                data-id=${data.id}
+                data-value=${data.value}
+              >
+                ${data.label}
+
+                ${feather.icons.x.toSvg({'width': 10, 'height': 10, 'data-deletable': '', 'data-button': '' })}
+              </span>
+            `)
+          }
+        }
+      }
+    })
   }
 }
