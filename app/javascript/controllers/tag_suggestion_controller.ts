9e3f55aa4dd761db0f89aa5ddda2f96beee63839
kimihito <tatsurotamashiro@gmail.com>
2019-11-09 18:23:39 +0900

    wip: tag suggestion

---
+++ app/javascript/controllers/tag_suggestion_controller.ts
@@ -0,0 +1,15 @@
+import * as Stimulus from 'stimulus'
+
+import Tribute from 'tributejs'
+
+import { throttle } from 'lodash'
+
+import { extractHashtags } from 'twitter-text'
+
+const URL = "/my/projects.json"
+
+export default class extends Stimulus.Controller {
+  public suggestTag() {
+
+  }
+}
