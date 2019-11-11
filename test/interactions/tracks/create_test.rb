38894367ed4b88888afedcd504baa4059305b785
kimihito <tatsurotamashiro@gmail.com>
2019-11-11 17:20:49 +0900

    Remove unused test

--- test/interactions/tracks/create_test.rb
+++ test/interactions/tracks/create_test.rb
@@ -24,12 +24,4 @@ class Tracks::CreateTest < ActiveSupport::TestCase
       Tracks::Create.run(owner: @owner, raw_body: 'hello world')
     end
   end
-
-  # test "should not save without difference project" do
-  #   project = Project.create(title: 'title', owner: users(:idoneman2))
-  #   tag = Tag.create(name: 'hello', project: project)
-  #   assert_no_difference ["Track.count"] do
-  #     Tracks::Create.run(owner: @owner, raw_body: "##{tag.name} world")
-  #   end
-  # end
 end
