bfb8a4b5d737ee1c6ad8085991ab4c83bee72ff8
kimihito <tatsurotamashiro@gmail.com>
2019-12-01 21:57:00 +0900

    :wrench: Activate rollbar sidekiq and js integrations

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -3,19 +3,19 @@ Rollbar.configure do |config|
   # To disable in specific environments, set config.enabled=false.
 
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.use_sidekiq
 
-  # Here we'll disable in 'test':
+  config.js_enabled = ENV.has_value?('ROLLBAR_CLIENT_ACCESS_TOKEN')
+  config.js_options = {
+    accessToken: ENV['ROLLBAR_CLIENT_ACCESS_TOKEN'],
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: { environment: config.environment }
+  }
+
   if Rails.env.test? || Rails.env.development?
     config.enabled = false
-    config.use_sidekiq
-
-    config.js_enabled = true
-    config.js_options = {
-      accessToken: ENV['ROLLBAR_CLIENT_ACCESS_TOKEN'],
-      captureUncaught: true,
-      captureUnhandledRejections: true,
-      payload: { environment: config.environment }
-    }
+    config.js_enabled = false
   end
 
   config.environment = ENV['ROLLBAR_ENV'].presence || Rails.env
